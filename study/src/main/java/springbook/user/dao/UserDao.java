fe39382692f93ece7c47cb7f9be2410b9c332dbe
권일철 <ilcheol.kwon@navercorp.com>
2024-04-21 18:17:03 +0900

    1.1.3 main DAO test code

---
+++ study/src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,65 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+    public static void main(String[] args) throws SQLException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteShip");
+        user.setName("백기선");
+        user.setPassword("married");
+
+//        userDao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+
+    public void add(User user) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/study", "root", "dlfcjf2@");
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public User get(String id) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/study", "root", "dlfcjf2@");
+
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "select * from users where id = ?");
+        preparedStatement.setString(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+}
